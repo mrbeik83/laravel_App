48189029e6e668dd6eea0dbf6a0dd5b341a59840
mrbeik <rezabeikyazdi@gmail.com>
2025-07-19 15:11:07 +0330

    Change View Master

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,19 +1,31 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title', 'Unique Scarf')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    {{-- استایل‌های مشترک --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.rtl.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    {{-- استایل اختصاصی هر صفحه --}}
+    @stack('styles')
+</head>
 <body>
 
     {{-- هدر --}}
     @include('layout.header')
 
-    {{-- محتوای صفحه --}}
-    <main class="container mt-4">
+    {{-- محتوای هر صفحه --}}
+    <main>
         @yield('content')
     </main>
 
     {{-- فوتر --}}
     @include('layout.footer')
 
-    {{-- اسکریپت بوت‌استرپ --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+    @stack('scripts')
 </body>
 </html>
