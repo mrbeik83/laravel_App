4df722347291121dc046404c1797c852172f9ec5
mrbeik <rezabeikyazdi@gmail.com>
2025-08-06 10:54:22 +0330

    change function index

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index(){
         $products = Product::latest()->get();
-        return view('admin.dashboard',compact('products'));
+        $productCount = Product::count();
+        $customerCount = User::get()->where('isAdmin',null)->count();
+        return view('admin.dashboard',compact(['products','productCount','customerCount']));
     }
     public function editProduct($id){
         $product = Product::findOrFail($id);
-        
     }
 }
