afcaff38fafa27e4812b0826c1af09e6937d8a87
mrbeik <rezabeikyazdi@gmail.com>
2025-08-10 19:56:20 +0330

    Complete the registration process with your phone number and verification code.

--- app/Http/Controllers/Auth/VerificationCodeController.php
+++ app/Http/Controllers/Auth/VerificationCodeController.php
@@ -3,28 +3,32 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Models\VerificationCode;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class VerificationCodeController extends Controller
 {
-    public function showForm(){
+    public function showForm()
+    {
         return view('auth.login-sms');
     }
-    public function sendCode(Request $request){
-        $validator = validator::make($request->all(),[
-            'phone' => ['required' , 'regex:/^09[0-9]{9}$/']
-        ]);
+    public function sendCode(Request $request)
+    {
 
-        if($validator->fails()){
+        $validator = validator::make($request->all(), [
+            'phone' => ['required', 'regex:/^9[0-9]{9}$/']
+        ]);
+        if ($validator->fails()) {
             return redirect()->back()->withErrors($validator)->withInput();
         }
 
         $phone = $request->phone;
 
-        $code = random_int(100000,999999);
+        $code = random_int(100000, 999999);
 
         VerificationCode::create([
             'phone' => $phone,
@@ -34,6 +38,38 @@ class VerificationCodeController extends Controller
         ]);
         // فضا برای ارسال کد به مشتری
 
-        return redirect()->back()->with('success',"کد تایید برای $phone ارسال شد :$code");
+        return view('Auth/login-sms-code', compact(['phone', 'code']));
     }
+
+    public function verifyCode(Request $request)
+    {
+        
+        $request->validate([
+            'phone' => ['required', 'digits:10'],
+            'code' => ['required', 'digits:6']
+        ]);
+        $verification = VerificationCode::where('phone', $request->phone)
+            ->where('code', $request->code)
+            ->where('used', false)
+            ->where('expires_at', '>', now())
+            ->first();
+        
+        if (! $verification) {
+            return back()->withErrors(['code' => 'کد وارد شده اشتباه یا منقضی شده است']);
+        }
+
+        $verification->update([
+            'used' => true
+        ]);
+        
+        $user = User::firstOrCreate(
+            ['phone' => $request->phone],
+            ['userName' => 'کاربر' . substr($request->phone, -4)]
+        );
+
+        Auth::login($user);
+
+        return redirect()->route('dashboard');
+    }
+    public function resendCode() {}
 }
