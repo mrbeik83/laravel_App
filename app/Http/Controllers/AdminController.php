e638d112759f73196193fe28a4f3a57d00008835
mrbeik <rezabeikyazdi@gmail.com>
2025-08-14 03:49:09 +0330

    update index function and add function showCustomers

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\Slider;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -12,9 +13,15 @@ class AdminController extends Controller
         $products = Product::latest()->get();
         $productCount = Product::count();
         $customerCount = User::get()->where('isAdmin',null)->count();
-        return view('admin.dashboard',compact(['products','productCount','customerCount']));
+        $customers = User::get()->where('isAdmin',null);
+        $sliders = Slider::get();
+        return view('admin.dashboard',compact(['products','productCount','customerCount','customers','sliders']));
     }
     public function editProduct($id){
         $product = Product::findOrFail($id);
+    }
+    public function showCustomers(){
+        $customers = User::get()->where('isAdmin',null);
+        dd($customers);
     }
 }
